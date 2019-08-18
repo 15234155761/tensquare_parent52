c9c3a8d4338e29acd1ce97832d13dd5673743425
520821cc <1923224661@qq.com>
2019-08-18 15:28:53 +0800

    第三次3

--- tensquare_base/src/main/java/com/tensquare/base/pojo/Entity.java
+++ tensquare_base/src/main/java/com/tensquare/base/pojo/Entity.java
@@ -11,6 +11,7 @@ public class Entity {
      */
 
     private WeatherinfoBean weatherinfo;
+    private WeatherinfoBean ccc;
 
     public WeatherinfoBean getWeatherinfo() {
         return weatherinfo;
--- tensquare_base/src/main/java/com/tensquare/base/pojo/Label.java
+++ tensquare_base/src/main/java/com/tensquare/base/pojo/Label.java
@@ -5,11 +5,15 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import java.io.Serializable;
 
+/**
+ * 类数组
+ */
 @Entity
 @Table(name = "tb_label")
 public class Label implements Serializable{
     @Id
     private String id;
+
     private String labelname;//标签名称
     private String state;//状态
     private Long count;//使用数量
